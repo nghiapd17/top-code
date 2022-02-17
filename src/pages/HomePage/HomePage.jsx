482d9bcea054b95574249cb78195c2dfb4e7a62b
Phạm Đại Nghĩa <nghiapd@smartosc.com>
2022-02-17 14:01:16 +0700

    create app

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import useStyles from "./HomePage.style";
+
+const HomePage = () => {
+  const classes = useStyles();
+  return <div className={classes.text}>HomePage</div>;
+};
+
+export default HomePage;
