482960c9fb994e21a26540ecb43b3631f74cadeb
Phạm Đại Nghĩa <nghiapd@smartosc.com>
2022-02-19 21:21:23 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import HomePage from "./pages/HomePage";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar /> */}
       <HomePage />
     </div>
   );
